637b192ded2f0d81ad92fa97f100fae3b897ce9a
Billy Fallon <wfallon@pingry.org>
2016-03-29 11:06:31 -0400

    Final Commit Functional Programming
    Billy Fallon functional programming final commit all methods 3/29/16

--- RacketWork.rkt
+++ RacketWork.rkt
@@ -1,13 +1,14 @@
 #lang racket
 
+;1) ToCelcius
 (define (to-celcius x) (/ (- x 32.0) 1.8))
 
 (to-celcius 32)
 (to-celcius 86)
 (to-celcius 5)
 
+;2) IsLeapYear
 (define (is-leap-year x) (if (zero? (modulo x 4)) (if (zero? (modulo x 100)) (if (zero? (modulo x 400)) true false) true) false))
-
 
 
 (is-leap-year 2016)
@@ -16,6 +17,8 @@
 (is-leap-year 1900)
 (is-leap-year 1700)
 
+
+;3) Countdown
 (define (countdown x)
   (cond
     [(< x 1) "Blastoff"]
@@ -23,30 +26,43 @@
     )
   )
 
+
 (countdown 4)
 
 
+;4) Summation (Sum digits)
+(define (sumdigits num)
+  (if (< num 10) num (+ (modulo num 10) (sumdigits (quotient num 10)))))  
+
+
+(sumdigits 1234567)
+
+
+;5) NumDigits
 (define (numdigits num)
     (if (< num 10) 1 (+ 1 (numdigits (quotient num 10)))))                   
 
 (numdigits 1234567)
 
-(define (sumdigits num)
-  (if (< num 10) num (+ (modulo num 10) (sumdigits (quotient num 10)))))  
 
-(sumdigits 1234567)
 
+;Test Lists
 (cons 1 2)
 
 (cons 4 (cons 3 empty))
 
 
+
+;6) CreateList
 (define (create-list size)
   (if (< size 1) empty (cons size (create-list ( - size 1)))))
 
 
 
+;7) LengthList
 (define (length-list list)
   (if (empty? list) 0 (+ 1 (length-list (rest list)))))
 
 (length-list (create-list 16))
+
+
